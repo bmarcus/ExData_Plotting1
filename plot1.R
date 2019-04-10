4141b2ac26d5ded3332ebf53c165a5941f8fc6bd
Brad Marcus <brad@movingshark.com>
2019-04-09 21:49:36 -0400

    initial commit

---
+++ plot1.R
@@ -0,0 +1,2 @@
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile="./household_power_consumption.zip", method="curl")
+# dt <- read.csv("./getdata_data_ss06hid.csv",header=TRUE)
