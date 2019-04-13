a3806dfe1c989e9b6fd8929686fe26fcf424095f
Brad Marcus <brad@movingshark.com>
2019-04-12 23:55:44 -0400

    Plot 1 works.

--- plot1.R
+++ plot1.R
@@ -1,16 +1,13 @@
+#######################################################
+# Exploratory Data Analysis - Project 1 - Plot 1
+#######################################################
 library(data.table)
-library("dplyr")
-
-# path <- getwd()
-# download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
- #             , destfile = paste(path, "dataFiles.zip", sep = "/"))
-# unzip(zipfile = "dataFiles.zip")
+library(dplyr)
 
 # Download the raw data file (zipped)
 download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
         destfile="./RAW_DATA/household_power_consumption.zip", 
         method="curl")
-# dt <- read.csv("./getdata_data_ss06hid.csv",header=TRUE)
 
 # unzip the data file and rename it locally
 unzip(zipfile = "./RAW_DATA/household_power_consumption.zip", exdir="./RAW_DATA")
@@ -25,4 +22,15 @@ newFile[,DateTime := as.Date(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
 #filter the data to only include dates 02-01-2007 through 02-02-2017
 newFile <- filter(newFile, 
         DateTime >= as.Date("2007-02-01 00:00:00"), 
-        DateTime < as.Date("2007-02-02 00:00:00"))
+        DateTime <= as.Date("2007-02-02 00:00:00"))
+
+# plot Global Active Power / Frequency
+hist(newFile$Global_active_power,  
+        main="Global Active Power", 
+        xlab="Global Active Power (kilowatts)", 
+        ylab="Frequency", 
+        col="Red")
+
+# copy plot to png file
+dev.copy(png,"./plot.png")
+dev.off()
