ee6caf38de5229be51f4405e5fe39b175ac1d917
Brad Marcus <brad@movingshark.com>
2019-04-13 00:20:57 -0400

    Cleaned up code.

--- plot1.R
+++ plot1.R
@@ -3,6 +3,7 @@
 #######################################################
 library(data.table)
 library(dplyr)
+library(lubridate)
 
 # Download the raw data file (zipped)
 download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
@@ -15,14 +16,13 @@ unzip(zipfile = "./RAW_DATA/household_power_consumption.zip", exdir="./RAW_DATA"
 # Read in the unzipped data file and put in tibble
 newFile <- fread("./RAW_DATA/household_power_consumption.txt", na.strings = "?")
 
-# create new DateTime column that combines Date and Time columns and put in new 
-# format d/%m/%Y %H:%M:%S
-newFile[,DateTime := as.Date(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+# cast Date column as date datatype to be used later for date comparison.
+newFile[,Date := as.Date(Date, format = "%d/%m/%Y")]
 
-#filter the data to only include dates 02-01-2007 through 02-02-2017
+# filter based on dates between 02-01-2007 and 02-02-2007
 newFile <- filter(newFile, 
-        DateTime >= as.Date("2007-02-01 00:00:00"), 
-        DateTime <= as.Date("2007-02-02 00:00:00"))
+        Date >= ymd("2007-02-01"), 
+        Date <= ymd("2007-02-02"))
 
 # plot Global Active Power / Frequency
 hist(newFile$Global_active_power,  
